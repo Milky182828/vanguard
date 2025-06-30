fbe4e9683048fe4cb03aeb8070a88eabd2c5f195
Milky <nazarlekomcev77@gmail.com>
2025-06-30 22:07:29 +0300

    умные штрафы обнова

--- Vanguard Helper.lua
+++ Vanguard Helper.lua
@@ -3,7 +3,7 @@
 script_name("Vanguard Helper")
 script_description('This is a Lua script helper for Rodina RP players who work in the MVD')
 script_author("Milky")
-script_version("0.8 alpha")
+script_version("0.9 alpha")
 
 require('lib.moonloader')
 require('encoding').default = 'CP1251'
